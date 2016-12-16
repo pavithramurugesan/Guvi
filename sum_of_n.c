a35200dd16c7f4dc2270f46391dd96e06236efa3
pavithra murugesan <pavithra.m275@gmail.com>
2016-12-16 14:55:52 +0530

    Add files via upload

---
+++ sum_of_n.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int sum=0,i=0,n;
+printf("Enter a number:");
+scanf("%d",&n);
+while(i<=n)
+{
+sum+=i;
+i++;
+}
+printf("Sum of %d is %d",n,sum);
+getch();
+}
