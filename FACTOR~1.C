03bb4e139f20f9db4d5365b0879f0abb480ac090
pavithra murugesan <pavithra.m275@gmail.com>
2016-12-17 12:49:27 +0530

    Add files via upload

---
+++ FACTOR~1.C
@@ -0,0 +1,26 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int fact,i,n;
+printf("Enter a number:");
+scanf("%d",&n);
+if(n==0)
+{
+return 1;
+}
+else
+{
+fact=1;
+i=1;
+while(i<=n)
+{
+fact=fact*i;
+i++;
+}
+printf("%d",fact);
+}
+getch();
+//return 0;
+}
